4ae4eac84a6c9af61f9d192ab7312e0c800e5248
Peter Nijssen <peter@peternijssen.nl>
2024-06-17 21:15:33 +0200

    Packet Types

--- custom_components/bold_ble/lib_files/bold_types.py
+++ custom_components/bold_ble/lib_files/bold_types.py
@@ -1,4 +1,5 @@
-from typing import TypedDict
+
+from typing import Literal, TypedDict
 
 
 class BoldBleDeviceInfo(TypedDict):
@@ -10,3 +11,30 @@ class BoldBleDeviceInfo(TypedDict):
     events_available: bool
     should_time_sync: bool
     is_in_dfu_mode: bool
+
+BoldBlePacketTypes = {
+    "ResultSuccess": 0x00,
+    "StartHandshake": 0xa0,
+    "HandshakeResponse": 0xa1,
+    "HandshakeClientResponse": 0xa2,
+    "HandshakeFinishedResponse": 0xa3,
+    "Command": 0xa4,
+    "CommandAck": 0xa5,
+    "LocalCommand": 0xa6,
+    "LocalCommandResponse": 0xa7,
+    "DeliverMessages": 0xb0,
+    "DialogServer": 0xc0,
+    "DialogDevice": 0xc1,
+    "Event": 0xd0,
+    "EventAck": 0xd1,
+    "EventAckResponse": 0xd2,
+    "ClientBlocked": 0xfd,
+    "HandshakeExpired": 0xfe,
+    "EncryptionError": 0xff,
+}
+
+BoldBlePacketType = Literal[
+    0x00, 0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5,
+    0xa6, 0xa7, 0xb0, 0xc0, 0xc1, 0xd0, 0xd1,
+    0xd2, 0xfd, 0xfe, 0xff
+]
