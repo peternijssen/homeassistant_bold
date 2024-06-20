e9a3a1f023e462e07384323d2453c7d047367651
Peter Nijssen <peter@peternijssen.nl>
2024-06-20 20:55:12 +0200

    Added comments from the original component

--- custom_components/bold_ble/lib_files/bold_types.py
+++ custom_components/bold_ble/lib_files/bold_types.py
@@ -1,7 +1,7 @@
+"""The Bold BLE integration types."""
 
-from typing import Literal, TypedDict
 from dataclasses import dataclass
-from typing import Any
+from typing import Any, TypedDict
 
 from bleak.backends.device import BLEDevice
 
@@ -30,28 +30,37 @@ class BoldAdvertisement:
     active: bool = False
 
 BoldBlePacketTypes = {
+    # Generic success packet.
     "ResultSuccess": 0x00,
-    "StartHandshake": 0xa0,
-    "HandshakeResponse": 0xa1,
-    "HandshakeClientResponse": 0xa2,
-    "HandshakeFinishedResponse": 0xa3,
-    "Command": 0xa4,
-    "CommandAck": 0xa5,
+
+    # Handshake packets.
+    "StartHandshake": 0xa0, # app => lock, payload = handshake.payload (57 bytes)
+    "HandshakeResponse": 0xa1, # lock => app, payload = nonce (13 bytes) + server challenge (8 bytes) (total 21 bytes)
+    "HandshakeClientResponse": 0xa2, # app => lock, payload = encrypted client response + client challenge (16 bytes)
+    "HandshakeFinishedResponse": 0xa3, # lock => app, payload = encrypted server response (9 bytes)
+
+    # Command packets.
+    "Command": 0xa4, # app => lock, payload = encrypted command.payload (46 bytes)
+    "CommandAck": 0xa5, # lock => app, payload = encrypted result + activation time (4 bytes)
+
+    # Local packets (Connect Hub getting APs and setting wifi credentials).
     "LocalCommand": 0xa6,
     "LocalCommandResponse": 0xa7,
+
+    # Message packets.
     "DeliverMessages": 0xb0,
+
+    # Dialog packets (pairing for installation, time sync).
     "DialogServer": 0xc0,
     "DialogDevice": 0xc1,
+
+    # Event packets.
     "Event": 0xd0,
     "EventAck": 0xd1,
     "EventAckResponse": 0xd2,
-    "ClientBlocked": 0xfd,
-    "HandshakeExpired": 0xfe,
-    "EncryptionError": 0xff,
-}
 
-BoldBlePacketType = Literal[
-    0x00, 0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5,
-    0xa6, 0xa7, 0xb0, 0xc0, 0xc1, 0xd0, 0xd1,
-    0xd2, 0xfd, 0xfe, 0xff
-]
+    # Error packets (single-byte packets, no payload).
+    "ClientBlocked": 0xfd, # lock => app
+    "HandshakeExpired": 0xfe, # lock => app
+    "EncryptionError": 0xff, # lock => app
+}
